757a66352c08f0888fd434757b13022e83753635
Felix Angell <felixangell@otzaf.org>
2019-07-20 15:19:48 +0100

    grammar changes, added pages with hash routing

--- src/home.js
+++ src/home.js
@@ -6,8 +6,7 @@ export default () => (
             <h1>Hello, World!</h1>
             <p className="mission">
                 Otzaf is <strong>privacy</strong> first. This is reflected in our software and our revenue model.
-                <br/> Our mission is to provide software for the people, not for profit.
-                <em>Most</em> of our software is open source to give back to the community.
+                Our mission is to provide software for the people - not profit. Most of our software is open source to give back to the community.
             </p>
         </article>
 
@@ -19,7 +18,7 @@ export default () => (
                 Our primary revenue stream will be from a variety of alternative paid-for software solutions.
             </p>
             <p>
-                Particular problems we're interested in tackling are: data storage, email, text editors, web browsers, search engines,
+                Particular problems we're interested in tackling are data storage, email, text editors, web browsers, search engines,
                 and more. We want to provide a service that is worth paying for, where you aren't the product.
             </p>
         </article>
@@ -33,7 +32,7 @@ export default () => (
                 on the <a href='https://github.com/otzaf/otzaf.org'>GitHub</a> page.
             </p>
             <p>
-                All of the open source projects we have are published on GitHub &mdash; any contributions
+                All of the open source projects we have can be found on GitHub &mdash; any contributions
                 are welcome.
             </p>
         </article>
