2442f51cdb090038a9228b902cd9ed34c8fb26cc
Felix Angell <felixangell@otzaf.org>
2019-09-12 22:07:21 +0100

    updated contact info

--- src/contact.js
+++ src/contact.js
@@ -5,8 +5,9 @@ export default () => (
     <article>
       <h1>Contact Us</h1>
       <p>
-                If you want to get hold of us, send an email to queries@hugobrains.com. Any bug reports can be
-                made to their relevant projects on our <a href='https://github.com/otzaf'>GitHub organisation</a>.
+        If you want to get hold of us, send an email to queries@otzaf.org. Any bug reports can be
+        made to their relevant projects on our <a href='https://github.com/otzaf'>GitHub organisation</a>. Security related
+        bugs can be emailed to us at security@otzaf.org.
       </p>
     </article>
   </main>
